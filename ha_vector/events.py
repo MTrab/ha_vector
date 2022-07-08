420e17850535d746d1bb59f16e4be24dd50eaef4
Malene <malene@trab.dk>
2022-07-08 10:44:59 +0200

    Removing warning for unknown event type

--- ha_vector/events.py
+++ ha_vector/events.py
@@ -251,7 +251,8 @@ class EventHandler:
                     unpackaged_event_key, unpackaged_event_data = self._unpackage_event('event_type', evt.event)
                     await self.dispatch_event_by_name(unpackaged_event_data, unpackaged_event_key)
                 except TypeError:
-                    self.logger.warning('Unknown Event type')
+                    # self.logger.warning('Unknown Event type')
+                    pass
         except CancelledError:
             self.logger.debug('Event handler task was cancelled. This is expected during disconnection.')
 
